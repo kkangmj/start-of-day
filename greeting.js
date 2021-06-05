c6ee4535bc2867159ad54ab8cb64c2fdb1e2ef5e
minju <bbak0402dd@gmail.com>
2021-06-05 23:03:26 +0900

    style: Changed styles of greeting
    - greeting과 관련된 부분의 스타일 수정

--- greeting.js
+++ greeting.js
@@ -2,8 +2,9 @@ const form = document.querySelector(".js-form");
 const input= form.querySelector("input");
 const greeting = document.querySelector(".js-greeting");
 
-const USER_LS = "min";
+const USER_LS = "USERNAME";
 const SHOWING_CN = "showing";
+const SHOWING_NAME_CN = "showing-name";
 
 function init(){
     loadName();
@@ -16,9 +17,10 @@ function saveName(text){
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = input.value;
-    saveName(currentValue);
-    paintGreeting(currentValue);
-
+    if (currentValue.length > 1){
+        saveName(currentValue);
+        paintGreeting(currentValue);
+    }
 }
 
 function askForName(){
@@ -26,10 +28,32 @@ function askForName(){
     form.addEventListener("submit", handleSubmit);
 }
 
+function checkGreetingKeyword(){
+    
+    const greetingWord = [
+        [20, 'Good night,'],
+        [18, 'Good evening,'],
+        [12, 'Good afternoon,'],
+        [8, 'Good morning,'],
+        [6, 'Whoa, your an early bird!'],
+        [0, 'Working late...']
+    ]
+
+    const currentHour = new Date().getHours();
+
+    for (let i=0; i< greetingWord.length ;i++){
+        if (currentHour >= greetingWord[i][0]){
+            return greetingWord[i][1];
+        }
+    }
+
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello, ${text}!`;
+    greeting.classList.add(SHOWING_CN, SHOWING_NAME_CN);
+    const greetingWord = checkGreetingKeyword();
+    greeting.innerText = `${greetingWord} ${text}`;
 }
 
 function loadName(){
